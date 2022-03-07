4afb3f924766776acc37390679d83782e84054c0
unknown <Josh@Josh.IMGSOL.co.zw>
2022-03-07 19:45:53 +0200

    files for github pages

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import React, { Component } from "react";
+import { SearchBox } from "./components/search-box/search-box.component";
+import { CardList } from "./components/card-list/card-list.component";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
